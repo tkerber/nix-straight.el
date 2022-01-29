08d75e5651cb52f8a07e03408ed19e04bee07505
ckie <git-525ff67@ckie.dev>
2022-01-29 19:03:21 +0200

    straight: update & patch

--- straight/default.nix
+++ straight/default.nix
@@ -3,10 +3,11 @@
 trivialBuild rec {
   pname = "straight.el";
   ename = pname;
+  patches = [ ./nogit.patch ];
   src = fetchFromGitHub {
     owner = "raxod502";
     repo = "straight.el";
-    rev = "e1390a933b6f5a15079d6dec91eac97a17aad10c";
-    sha256 = "sha256-fzIvJ5sDNDtF0w8t1WtM2SpnWT8zM2RG/EiSthy1URU=";
+    rev = "af5437f2afd00936c883124d6d3098721c2d306c";
+    sha256 = "sha256-Mh555zVj4joLqnCzDzv4lu9RKRnmtYqVR9SbNs7A6tY=";
   };
 }
