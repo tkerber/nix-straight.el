ab09cca6836c8827f47bb75dae138826327eda7c
ckie <git-525ff67@ckie.dev>
2022-07-01 17:04:03 +0300

    straight: update

--- straight/default.nix
+++ straight/default.nix
@@ -7,7 +7,7 @@ trivialBuild rec {
   src = fetchFromGitHub {
     owner = "raxod502";
     repo = "straight.el";
-    rev = "af5437f2afd00936c883124d6d3098721c2d306c";
-    sha256 = "sha256-Mh555zVj4joLqnCzDzv4lu9RKRnmtYqVR9SbNs7A6tY=";
+    rev = "4517e118ee43f849f708025dbb2cf4f281793121";
+    sha256 = "sha256-KIOdXoujXZkhWUR+Sql9FOCccnwGCUMAS0zlAQR8sEU=";
   };
 }
